b6e9c2670e1f599c27d4d76139431c292d9531c5
Otto <z5116011@student.unsw.edu.au>
2018-07-26 10:52:39 +1000

    adding comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+/**
+ * A simple java program that prints a hello world message to the console
+ *
+ */
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to COMP2511!");
